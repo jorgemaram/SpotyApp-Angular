4fe7826e58313f8cb17114a79603557ff7f404ea
Jorge Martín <jorgemaram@gmail.com>
2021-05-10 13:39:26 +0200

    use of pipes for avoiding noimage problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,13 +15,17 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 //import routes
 import { ROUTES } from './app.routes';
 
+// Pipes
+import { NoimagePipe } from './pipes/noimage.pipe';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SearchComponent,
     ArtistComponent,
-    NavbarComponent
+    NavbarComponent,
+    NoimagePipe
   ],
   imports: [
     BrowserModule,
